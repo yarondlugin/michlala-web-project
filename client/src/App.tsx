31aaca7bf8d0bde68d612cb234b15e440e4c6387
Yaron Dlugin <yaron206@gmail.com>
2025-02-03 21:41:21 +0200

    feat(client): added basic configuration for tanstack router like in the docs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './App.css';
 import { IsAlive } from './components/IsAlive';
 
 const queryClient = new QueryClient();
---
+++ client/src/routes/about.tsx
@@ -0,0 +1,12 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/about')({
+  component: About,
+})
+
+function About() {
+  return <>
+  	<div className="p-2">Hello from About!</div>
+	<button>Click me</button>
+  </>
+}
