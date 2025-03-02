7cb0082144c236539124b35a05f631d522172594
Yaron Dlugin <yaron206@gmail.com>
2025-03-02 19:51:38 +0200

    feat(hooks): add useDialog hook

--- client/src/hooks/useDialog.tsx
+++ client/src/hooks/useDialog.tsx
@@ -1,6 +1,7 @@
 import { Dialog } from '@base-ui-components/react/dialog';
 import styles from './useDialog.module.css';
 import { Box, Button, Card } from '@mui/material';
+import { useState } from 'react';
 
 interface Props {
     title?: string;
@@ -21,6 +22,18 @@ export const useDialog = ({
     onConfirm = () => {},
     openText = '',
 }: Props) => {
+    const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+
+	const handleConfirm = async () => {
+		await onConfirm();
+		await setDialogOpen(false);
+	};
+
+	const handleCancel = async () => {
+		await onCancel();
+		await setDialogOpen(false);
+	};
+
     const dialogComponent = (
         <Dialog.Portal keepMounted>
             <Dialog.Backdrop className={styles.Backdrop} />
@@ -37,11 +50,11 @@ export const useDialog = ({
                     <Dialog.Title>{title}</Dialog.Title>
                     <Dialog.Description>{description}</Dialog.Description>
                     <Box sx={{ marginTop: '1rem', width: '100%', display: 'flex', justifyContent: 'space-around' }}>
-                        {confirmText && <Button variant="contained" onClick={onConfirm}>
+                        {confirmText && <Button variant="contained" onClick={handleConfirm}>
                             {confirmText}
                         </Button>}
                         {confirmText && cancelText && <Box sx={{ width: '5rem' }} />}
-                        {cancelText && <Dialog.Close render={<Button onClick={onCancel}>{cancelText}</Button>} />}
+                        {cancelText && <Dialog.Close render={<Button onClick={handleCancel}>{cancelText}</Button>} />}
                     </Box>
                 </Card>
             </Dialog.Popup>
@@ -51,6 +64,8 @@ export const useDialog = ({
     const openCompoent = <Dialog.Trigger render={<Button variant="contained">{openText}</Button>} />;
 
     return {
+		dialogOpen,
+		setDialogOpen,
         dialogComponent,
         openCompoent,
     };
