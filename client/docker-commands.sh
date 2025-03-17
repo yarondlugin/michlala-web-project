2cab1522aaeede1db1fd6d55304d4840d974dff6
Yaron Dlugin <yaron206@gmail.com>
2025-03-17 12:04:52 +0200

    feat: dockerfile for server

--- client/docker-commands.sh
+++ client/docker-commands.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker build -t web-client .
+sudo docker build -t client:latest .
+sudo docker build -t server:latest .
 
-docker run -p 443:443 -v /etc/ssl/certs:/etc/nginx/ssl/certs -v /etc/ssl/private:/etc/nginx/ssl/private web-client
+sudo docker run -d -p 443:443 -v /etc/ssl/certs:/etc/nginx/ssl/certs -v /etc/ssl/private:/etc/nginx/ssl/private client:latest
+sudo docker run -d -p 443:443 -v /etc/ssl/certs:/etc/ssl/certs -v /etc/ssl/private:/etc/ssl/private server:latest
