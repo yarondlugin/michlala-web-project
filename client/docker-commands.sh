e1a35019f6f372140b38838d9e37b79dbbdd72ad
Yaron Dlugin <yaron206@gmail.com>
2025-03-17 12:17:12 +0200

    add env to docker commands

--- client/docker-commands.sh
+++ client/docker-commands.sh
@@ -2,5 +2,5 @@
 sudo docker build -t client:latest .
 sudo docker build -t server:latest .
 
-sudo docker run -d -p 443:443 -v /etc/ssl/certs:/etc/nginx/ssl/certs -v /etc/ssl/private:/etc/nginx/ssl/private client:latest
-sudo docker run -d -p 8080:8080 -v /etc/ssl/certs:/etc/ssl/certs -v /etc/ssl/private:/etc/ssl/private server:latest
+sudo docker run -d -p 443:443 -v /etc/ssl/certs:/etc/nginx/ssl/certs -v /etc/ssl/private:/etc/nginx/ssl/private --env-file .env client:latest
+sudo docker run -d -p 8080:8080 -v /etc/ssl/certs:/etc/ssl/certs -v /etc/ssl/private:/etc/ssl/private --env-file .env server:latest
