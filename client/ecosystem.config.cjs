724e0aadb419a9748246e52980d42350665f3c51
yaron <yaron>
2025-03-18 01:00:36 +0200

    feat(pm2): services running correctly with pm2

--- client/ecosystem.config.cjs
+++ client/ecosystem.config.cjs
@@ -4,14 +4,8 @@ module.exports = {
     apps: [
         {
             name: `client`,
-			script: "pnpm",
-      		args: "preview",
-            env_production: {
-                VITE_SERVER_URL: 'https://localhost:8080',
-                VITE_GOOGLE_CLIENT_ID: '',
-                VITE_POSTS_PER_PAGE: 20,
-                NODE_ENV: 'production',
-            },
+			      script: "pnpm",
+      		  args: "preview"
         },
     ],
 };
--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,17 +4,7 @@ module.exports = {
 	apps: [
 		{
 			name: `server`,
-			script: "build/index.js",
-			env_production: {
-				NODE_ENV: "production",
-				GOOGLE_CLIENT_ID: "",
-				GOOGLE_CLIENT_SECRET: "",
-				AI_API_URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash",
-				AI_API_KEY: "",
-				KEY_PATH: "server/localCerts/key.key",
-				CERT_PATH: "server/localCerts/cert.crt",
-				POST_GENERATION_BATCH: 50
-			},
+			script: "build/index.js"
 		},
 	],
 };
