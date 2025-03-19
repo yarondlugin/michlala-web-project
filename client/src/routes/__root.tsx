1ed528014dda53bde591eca86d8365dcb952254a
Yaron Dlugin <yaron206@gmail.com>
2025-03-19 23:27:22 +0200

    fix: remove devtools

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from '@emotion/react';
 import { Box, createTheme, CssBaseline } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { CookiesProvider } from 'react-cookie';
 import { ErrorCard } from '../components/ErrorCard';
 import { PageBox } from '../components/PageBox';
@@ -26,7 +25,6 @@ export const Route = createRootRoute({
                 <QueryClientProvider client={queryClient}>
                     <CookiesProvider>
                         <Outlet />
-                        <TanStackRouterDevtools />
                     </CookiesProvider>
                 </QueryClientProvider>
             </ThemeProvider>
